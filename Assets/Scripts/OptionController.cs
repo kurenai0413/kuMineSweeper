b4216fd46fedc96ffaa23e513ad589c5d65a6cf5
kurenai0413 <kurenai0413@hotmail.com>
2016-12-06 22:07:52 +0800

    Add button actions of option menu.

---
+++ Assets/Scripts/OptionController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionController : MonoBehaviour {
+
+	public void SelectLevel()
+    {
+        string level
+            = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+
+        switch (level)
+        {
+            case "Easy":
+                Debug.Log("Easy~");
+                break;
+            case "Medium":
+                Debug.Log("Medium~~");
+                break;
+            case "Hard":
+                Debug.Log("Hard~~~");
+                break;
+        }
+    }
+
+    public void backMainMenu()
+    {
+        Application.LoadLevel("MineSweeper");
+    }
+}
