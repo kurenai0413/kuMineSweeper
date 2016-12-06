2ecbe00eaf5940299df1e1bb8ece8cce09e8d08b
kurenai0413 <kurenai0413@hotmail.com>
2016-12-06 21:44:48 +0800

    First commit. Complete scene change by press button.

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+
+	public void PlayGame()
+    {
+        Application.LoadLevel("GamePlay");
+    }
+
+    public void GotoOption()
+    {
+        Application.LoadLevel("OptionMenu");
+    }
+}
